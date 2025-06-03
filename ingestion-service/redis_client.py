256bfe4faef579f2e5e9ecf3a86bed18217e249b
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-06-04 00:04:51 +0300

    redis fixes

--- ingestion-service/redis_client.py
+++ ingestion-service/redis_client.py
@@ -1,4 +1,7 @@
-# redis_client.py
-
+import os
 import redis
-r = redis.Redis(host="localhost", port=6379, db=0)
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", "6379"))
+
+r = redis.Redis(host=redis_host, port=redis_port)
